7e8d41ab45f48cd96e15c5afde0051f0062f7479
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-08-23 14:36:49 +1000

    Tester updated

--- src/Tester.java
+++ src/Tester.java
@@ -4,11 +4,26 @@ import java.util.Random;
 public class Tester {
     public static void main(String[] args) {
         int length = 9;
-        int[] arr = new int[length];
+        Integer[] arr = new Integer[length];
         Random rand = new Random();
         for(int i = 0; i<length; i++){
-            arr[i] = rand.nextInt(-2000,2000);
+            arr[i] = rand.nextInt(0,2000);
         }
-        System.out.println("Given array: "+ Arrays.toString(arr));
+        System.out.println("Given array:");
+        System.out.println(Arrays.toString(arr));
+
+        //BubbleSortOptimised.bubbleSortAsc(arr);
+        //System.out.println("After bubble sort:");
+
+        //SelectionSort.selectionSortAsc(arr);
+        //System.out.println("After selection sort:");
+
+        //InsertionSort.insertionSortAsc(arr);
+        //System.out.println("After insertion sort:");
+
+        MergeSort.mergeSortAsc(arr);
+        System.out.println("After merge sort:");
+
+        System.out.println(Arrays.toString(arr));
     }
 }
