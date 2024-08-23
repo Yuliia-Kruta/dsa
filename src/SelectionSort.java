1d026ce16be407f7bba67a27a51d80ccb620a905
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-08-23 14:36:04 +1000

    Selection sort added

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SelectionSort {
+import java.util.Arrays;
+
+/*
+The algorithm repeatedly selects the smallest (or largest) element from the unsorted portion of the list
+and swaps it with the first element of the unsorted part. This process is repeated for the remaining unsorted
+portion until the entire list is sorted.
+
+Time Complexity: O(N^2)
+Auxiliary Space: O(1)
+*/
+public class SelectionSort {
+    public static <T extends Comparable<T>> void selectionSortAsc(T[] arr){
+        int n = arr.length;
+        T temp;
+        for(int i=0; i<n-1; i++){
+            int min = i;
+            for(int j=i+1;j<n; j++){
+                if(arr[j].compareTo(arr[min])<0){
+                    min = j;
+                }
+            }
+            temp = arr[i];
+            arr[i]=arr[min];
+            arr[min]= temp;
+        }
+    }
 }
